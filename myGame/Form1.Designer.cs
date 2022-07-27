67a753cd3f3fc7352a76c1aa9c8be8d44e912fe9
RezaHeidary <rrezah43388@gmail.com>
2022-07-27 11:01:01 +0430

    Add the pro Game

---
+++ myGame/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace myGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.تنظیماتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تکنفرهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.دونفرهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.خروجToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.دربارهماToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn1.Location = new System.Drawing.Point(15, 60);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(173, 120);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn2.Location = new System.Drawing.Point(194, 60);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(173, 120);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn3.Location = new System.Drawing.Point(373, 60);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(173, 120);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn6.Location = new System.Drawing.Point(373, 186);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(173, 120);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn7.Location = new System.Drawing.Point(15, 312);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(173, 120);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn8.Location = new System.Drawing.Point(194, 312);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(173, 120);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn9.Location = new System.Drawing.Point(373, 312);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(173, 120);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(572, 206);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(142, 80);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Restart";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn5.Location = new System.Drawing.Point(194, 186);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(173, 120);
+            this.btn5.TabIndex = 10;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn4.Location = new System.Drawing.Point(15, 186);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(173, 120);
+            this.btn4.TabIndex = 11;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تنظیماتToolStripMenuItem,
+            this.دربارهماToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(733, 28);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // تنظیماتToolStripMenuItem
+            // 
+            this.تنظیماتToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تکنفرهToolStripMenuItem,
+            this.دونفرهToolStripMenuItem,
+            this.خروجToolStripMenuItem});
+            this.تنظیماتToolStripMenuItem.Name = "تنظیماتToolStripMenuItem";
+            this.تنظیماتToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.تنظیماتToolStripMenuItem.Text = "تنظیمات";
+            // 
+            // تکنفرهToolStripMenuItem
+            // 
+            this.تکنفرهToolStripMenuItem.Name = "تکنفرهToolStripMenuItem";
+            this.تکنفرهToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D1)));
+            this.تکنفرهToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.تکنفرهToolStripMenuItem.Text = "تک نفره";
+            this.تکنفرهToolStripMenuItem.Click += new System.EventHandler(this.تکنفرهToolStripMenuItem_Click);
+            // 
+            // دونفرهToolStripMenuItem
+            // 
+            this.دونفرهToolStripMenuItem.Name = "دونفرهToolStripMenuItem";
+            this.دونفرهToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D2)));
+            this.دونفرهToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.دونفرهToolStripMenuItem.Text = "دو نفره";
+            this.دونفرهToolStripMenuItem.Click += new System.EventHandler(this.دونفرهToolStripMenuItem_Click);
+            // 
+            // خروجToolStripMenuItem
+            // 
+            this.خروجToolStripMenuItem.Name = "خروجToolStripMenuItem";
+            this.خروجToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.خروجToolStripMenuItem.Text = "خروج";
+            this.خروجToolStripMenuItem.Click += new System.EventHandler(this.خروجToolStripMenuItem_Click);
+            // 
+            // دربارهماToolStripMenuItem
+            // 
+            this.دربارهماToolStripMenuItem.Name = "دربارهماToolStripMenuItem";
+            this.دربارهماToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.دربارهماToolStripMenuItem.Text = "درباره ما";
+            this.دربارهماToolStripMenuItem.Click += new System.EventHandler(this.دربارهماToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 485);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem تنظیماتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تکنفرهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem دونفرهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem خروجToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem دربارهماToolStripMenuItem;
+    }
+}
+
---
+++ myGame/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        int ch = 0;
+        Button btn;
+        bool flag = true;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (flag)
+                donafre();
+            else
+                taknafre();
+
+
+        }
+
+    
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+     
+
+
+        bool isWin(string str)
+        {
+
+            if (btn1.Text == str && btn2.Text == str && btn3.Text == str)
+            {
+                MessageBox.Show(str + " is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+
+            }
+            else if (btn4.Text == str && btn5.Text == str && btn6.Text == str)
+            {
+                MessageBox.Show(str + " is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+            }
+            else if (btn7.Text == str && btn8.Text == str && btn9.Text == str)
+            {
+                MessageBox.Show(str + " is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+            }
+            else if (btn7.Text == str && btn1.Text == str && btn4.Text == str)
+            {
+                MessageBox.Show(str + "is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+            }
+            else if (btn3.Text == str && btn6.Text == str && btn9.Text == str)
+            {
+                MessageBox.Show(str + "is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+            }
+            else if (btn5.Text == str && btn2.Text == str && btn8.Text == str)
+            {
+                MessageBox.Show(str + " is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+            }
+            else if (btn5.Text == str && btn3.Text == str && btn7.Text == str)
+            {
+                MessageBox.Show(str + " is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+            }
+            else if (btn1.Text == str && btn5.Text == str && btn9.Text == str)
+            {
+                MessageBox.Show(str + " is Win !!!!!!!!");
+                reset();
+                return true;
+
+
+            }
+
+            return false;
+
+
+
+        }
+
+        void reset()
+        {
+            btn1.Text = btn2.Text = btn3.Text = btn4.Text = btn5.Text = btn6.Text = btn7.Text = btn8.Text = btn9.Text = "";
+            btn1.Enabled = btn2.Enabled = btn3.Enabled = btn4.Enabled = btn5.Enabled = btn6.Enabled = btn7.Enabled = btn8.Enabled = btn9.Enabled = true;
+        }
+
+        void donafre()
+        {
+
+            if (ch % 2 == 0)
+
+                btn.Text = "X";
+            else
+                btn.Text = "O";
+
+
+            btn.Enabled = false;
+            isWin(btn.Text);
+            ch++;
+        }
+
+
+
+
+
+        void taknafre()
+        {
+   
+                btn.Text = "X";
+                btn.Enabled = false;
+
+
+            int botch=0;
+            bool gg = false;
+            Random or = new Random();
+            Button[] btnList = { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            int rand = or.Next(0, 9);
+
+         
+            while (botch<9)
+            {
+                botch++;
+                if (btnList[rand].Text == "")
+                {
+                    btnList[rand].Text = "O";
+                    btnList[rand].Enabled = false;
+                    gg=isWin(btnList[rand].Text);
+                    
+                    break;
+                }
+                rand = or.Next(0, 9);
+
+             
+
+            }
+            if(gg==false)
+            isWin(btn.Text);
+
+
+
+
+        }
+   
+
+        private void دونفرهToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            reset();
+
+        }
+
+        private void تکنفرهToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            reset();
+                  
+                       
+
+        }
+
+        private void خروجToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void دربارهماToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("رضا حیدری رشته نرم افزار دانشکده سروش ");
+        }
+
+    
+    }
+}
